c47151bd3f033c8f38a00563f6f86777ecf8b628
Kyhro <bellevillepaul03@gmail.com>
2023-12-14 15:14:22 +0100

    added cards displaying

--- FlashcardsApp.sh
+++ FlashcardsApp.sh
@@ -19,6 +19,27 @@ do
 -> # $col2" > ./data/cards/pair$count.md
 done < "$FILE"
 
+echo "Préparation de la session" 
+cards=$(ls ./data/cards/*.md | shuf -n 10) 
+for c in $cards
+do 
+    mv "$c" ./data/levels/1/
+done
+
+echo "Début de la session"
+for c in $(ls ./data/levels/1/*.md | shuf)
+do 
+    level=$(echo "$c" | cut -d '/' -f 4)
+    echo "$level"
+    #mdp "$c"
+    #echo "Avez vous trouvé la réponse ? (y/n)"
+    #read answer
+    #if [[ $answer -eq "y" ]] 
+    
+
+
+done
+
 #ls session/*/*.md | shuf
 #fait un échantillon de fichiers aléatoire parmi les boites
 
