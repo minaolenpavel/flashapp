3b3be185bd0e4e1d9cece0f69270bae9534542ac
Kyhro <bellevillepaul03@gmail.com>
2024-01-15 16:20:42 +0100

    Added new layer of subfolder, created base for stats, need to fix cleaning function

--- FlashcardsApp.sh
+++ FlashcardsApp.sh
@@ -8,6 +8,11 @@
 #ajouter un onglet statistiques
 #ne pas oublier de faire un vide dans la création de la session après la session, créer une boite session ?
 
+statistics()
+{
+    echo 
+}
+
 installer()
 {
     echo "Bienvenue dans FlashApp™"
@@ -20,6 +25,14 @@ installer()
         if [[ $answer == "y" ]]
         then
             mkdir -p ~/FlashApp/data/
+            echo -ne '#####                     (33%)\r'
+            sleep 1
+            touch ~/FlashApp/data/stats.txt
+            echo -ne '#############             (66%)\r'
+            sleep 1
+            mkdir ~/FlashApp/data/sets
+            echo -ne '#######################   (100%)\r'
+            echo -ne '\n'
             echo "FlashApp est désormais installé sur votre ordinateur"
             echo "Vous pouvez lancer le même programme pour l'utiliser"
             answered="true"
@@ -58,16 +71,16 @@ create_cards()
 {
     echo "Quel nom voulez-vous donner à votre set de cartes ?"
     read name
-    mkdir -p "$HOME"/FlashApp/data/"$name"/{levels,session}/
-    mkdir -p "$HOME"/FlashApp/data/"$name"/levels/{1,2,3,4}/
-    mkdir -p "$HOME"/FlashApp/data/"$name"/session/{1,2,3,4}/
+    mkdir -p "$HOME"/FlashApp/data/sets/"$name"/{levels,session}/
+    mkdir -p "$HOME"/FlashApp/data/sets/"$name"/levels/{1,2,3,4}/
+    mkdir -p "$HOME"/FlashApp/data/sets/"$name"/session/{1,2,3,4}/
     echo "Assurez vous d'avoir bien placé votre fichier .csv dans le dossier utilisateur"
     echo "Veuillez écrire le nom du fichier" 
     read file
     echo -ne '#####                     (33%)\r'
     sleep 1
-    cp "$HOME""/$file" "$HOME"/FlashApp/data/"$name"
-    FILE="$HOME/FlashApp/data/$name/$file"
+    cp "$HOME""/$file" "$HOME"/FlashApp/data/sets/"$name"
+    FILE="$HOME/FlashApp/data/sets/$name/$file"
     count=0
     first_line=$(head -n 1 "$FILE")
     echo -ne '#############             (66%)\r'
@@ -85,7 +98,7 @@ create_cards()
             [[ -n $col3 ]] && echo "# ${keys[2]} : $col3"
             [[ -n $col4 ]] && echo "# ${keys[3]} : $col4"
             [[ -n $col5 ]] && echo "# ${keys[4]} : $col5"
-        } > "$HOME/FlashApp/data/$name/levels/1/pair$count.md"
+        } > "$HOME/FlashApp/data/sets/$name/levels/1/pair$count.md"
     done < "$FILE"
     echo -ne '#######################   (100%)\r'
     echo -ne '\n'
@@ -101,20 +114,20 @@ test()
 cleaning()
 {
     ls $HOME/FlashApp/data/*/session/*/*.md
-    for c in $(ls $HOME/FlashApp/data/*/session/*/*.md)
+    for c in $(ls $HOME/FlashApp/data/sets/*/session/*/*.md)
     do 
-        set_name=$(echo "$c" | cut -d '/' -f 6)
-        level=$(echo "$c" | cut -d '/' -f 8)
-        mv "$c" "$HOME"/FlashApp/data/"$set_name"/levels/"$level"/
+        set_name=$(echo "$c" | cut -d '/' -f 7)
+        level=$(echo "$c" | cut -d '/' -f 9)
+        mv "$c" "$HOME"/FlashApp/data/sets/"$set_name"/levels/"$level"/
     done
 }
 
 session()
 {
     echo "Quel set de carte voulez-vous pratiquer ?"
-    for set in $(ls "$HOME"/FlashApp/data/)
+    for set in $(ls "$HOME"/FlashApp/data/sets)
     do  
-        echo $set
+        echo "$set"
     done
     read chosen_set
     echo "Préparation de la session" 
@@ -122,37 +135,40 @@ session()
     for i in $(seq 1 4)
     do
         echo $mv_amount
-        if [[ $(ls "$HOME"/FlashApp/data/"$chosen_set"/levels/"$i"/ | wc -l) -ge $mv_amount ]]
+        if [[ $(ls "$HOME"/FlashApp/data/sets/"$chosen_set"/levels/"$i"/ | wc -l) -ge $mv_amount ]]
         then 
-            for c in $(ls "$HOME"/FlashApp/data/"$chosen_set"/levels/$i/*.md | shuf -n $mv_amount)
+            for c in $(ls "$HOME"/FlashApp/data/sets/"$chosen_set"/levels/$i/*.md | shuf -n $mv_amount)
             do 
-                mv $c "$HOME"/FlashApp/data/"$chosen_set"/session/$i/
+                mv $c "$HOME"/FlashApp/data/sets/"$chosen_set"/session/$i/
             done
-        elif [[ $(ls "$HOME"/FlashApp/data/"$chosen_set"/levels/$i | wc -l) -ge 1 && $(ls "$HOME"/FlashApp/data/"$chosen_set"/levels/$i | wc -l) -lt $mv_amount ]]
+        elif [[ $(ls "$HOME"/FlashApp/data/sets/"$chosen_set"/levels/$i | wc -l) -ge 1 && $(ls "$HOME"/FlashApp/data/sets/"$chosen_set"/levels/$i | wc -l) -lt $mv_amount ]]
         then 
-            for c in $(ls "$HOME"/FlashApp/data/"$chosen_set"/levels/$i/*.md)
+            for c in $(ls "$HOME"/FlashApp/data/sets/"$chosen_set"/levels/$i/*.md)
             do 
-                mv $c "$HOME"/FlashApp/data/"$chosen_set"/session/$i/
+                mv $c "$HOME"/FlashApp/data/sets/"$chosen_set"/session/$i/
             done
         fi
         ((mv_amount--))
     done
-    nb_cards=$(ls "$HOME"/FlashApp/data/"$chosen_set"/session/*/*.md | wc -l)
+    nb_cards=$(ls "$HOME"/FlashApp/data/sets/"$chosen_set"/session/*/*.md | wc -l)
     if [[ $nb_cards -lt 10 ]]
     then 
         gap=$(expr 10 - "$nb_cards")
-        for c in $(ls "$HOME"/FlashApp/data/"$chosen_set"/levels/*/*.md | shuf -n "$gap")
+        for c in $(ls "$HOME"/FlashApp/data/sets/"$chosen_set"/levels/*/*.md | shuf -n "$gap")
         do 
-            level=$(echo "$c" | cut -d '/' -f 8)
-            mv "$c" "$HOME"/FlashApp/data/"$chosen_set"/session/"$level"/
+            level=$(echo "$c" | cut -d '/' -f 9)
+            mv "$c" "$HOME"/FlashApp/data/sets/"$chosen_set"/session/"$level"/
         done
     fi
     
     #####
     echo "Début de la session"
-    for c in $(ls "$HOME"/FlashApp/data/"$chosen_set"/session/*/*.md | shuf)
+    right=0
+    wrong=0
+    total=0
+    for c in $(ls "$HOME"/FlashApp/data/sets/"$chosen_set"/session/*/*.md | shuf)
     do 
-        level=$(echo "$c" | cut -d '/' -f 8)
+        level=$(echo "$c" | cut -d '/' -f 9)
         mdp "$c"
         echo "Avez vous trouvé la réponse ? (y/n)"
         answered="false"
@@ -161,23 +177,27 @@ session()
             read answer
             if [[ "$answer" == "y" ]] 
             then 
+                ((total++))
+                ((right++))
                 if [[ $level != 4 ]]
                 then 
-                    level=$(expr "$level" + 1)
-                    mv "$c" "$HOME"/FlashApp/data/"$chosen_set"/levels/"$level"/
-                    echo "carte $c dans la boite $level"
+                    ((level++ ))
+                    mv "$c" "$HOME"/FlashApp/data/sets/"$chosen_set"/levels/"$level"/
+                    echo "carte $c au niveau $level"
                 fi
                 answered="true"
 
             elif [[ "$answer" == "n" ]]
             then 
+                ((total++))
+                ((wrong++))
                 if [[ $level != 1 ]]
                 then 
-                    level=$(expr $level - 1)
-                    mv "$c" "$HOME"/FlashApp/data/"$chosen_set"/data/levels/"$level"/
-                    echo "carte $c dans la boite $level"
+                    ((level-- ))
+                    mv "$c" "$HOME"/FlashApp/data/sets/"$chosen_set"/data/levels/"$level"/
+                    echo "carte $c au niveau $level"
                 else 
-                    mv "$c" "$HOME"/FlashApp/data/"$chosen_set"/levels/"$level"
+                    mv "$c" "$HOME"/FlashApp/data/sets/"$chosen_set"/levels/"$level"
                 fi
                 answered="true"
             else 
@@ -185,11 +205,14 @@ session()
             fi
         done
     done
+    echo "$wrong"
+    echo "$total"
+    echo "$right"
 }
 
 main()
 {
-    nb_cards=$(ls "$HOME"/FlashApp/data/"$chosen_set"/session/*/*.md 2>/dev/null | wc -l)
+    nb_cards=$(ls "$HOME"/FlashApp/data/sets/*/session/*/*.md 2>/dev/null | wc -l)
     if [[ $nb_cards -ge 1 ]]
     then 
         echo "Vous n'avez pas fini votre session la dernière fois !"
@@ -222,6 +245,8 @@ main()
         echo "Veuillez faire un choix :"
         echo "1 - Session du jour"
         echo "2 - Créer un set de carte"
+        echo "3 - Voir mes statistiques"
+        echo "4 - Quitter le programme"
         read answer
         if [[ "$answer" == "1" ]]
         then 
@@ -230,6 +255,10 @@ main()
         elif [[ "$answer" == "2" ]]
         then 
             create_cards
+        elif [[ "$answer" == "4" ]]
+        then 
+            echo "Au revoir"
+            answered="true"
         else
             echo "Je n'ai pas compris votre demande, veuillez insérer une commande correcte."
         fi 
